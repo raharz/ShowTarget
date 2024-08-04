8febdeda29967c0718fe2a575a0c340e69e8cb34
raha <raharezaaeii@gmail.com>
2024-08-04 14:44:03 +0330

    add(script.js):set target and string

---
+++ script.js
@@ -0,0 +1,9 @@
+// start to search and find the target
+let string = "my name is raha",
+  target = "raha";
+if (true) {
+  console.log("welcome Back raha");
+}
+if (false) {
+  console.log("your name is not registered");
+}
